bcc74fa5e05ac6c3fad8e058a45f8db63d0e1931
Ayana Osawa <>
2019-09-25 16:26:17 -0400

    Update latest files

---
+++ src/compornents/SearchBar.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+class SearchBar extends React.Component {
+  state = { term: '' }
+
+  onInputChange = (event) => {
+    this.setState({ term: event.target.value })
+  }
+
+  onFormSubmit = (event) => {
+    // event.preventDefault()
+  }
+
+  render() { 
+    return (
+      <div className="search-bar ui segment" style={{marginTop: "2em"}}>
+        <form className="ui search" onSubmit={this.onFormSubmit}>
+          <div className="ui icon input fluid">
+            <input 
+              className="prompt" 
+              type="text" 
+              placeholder="Search videos..." 
+              value={this.state.term}
+              onChange={this.onInputChange}
+            />
+            <i className="search icon"></i>
+          </div>
+
+        </form>
+      </div>
+    )
+  }
+}
+ 
+export default SearchBar;
